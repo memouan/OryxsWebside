cdbe5e7e034ebdfa74621f0ba7e9c383bc819612
Ouannassi Mehdi <mehdi.ouannassi.mo@gmail.com>
2022-05-26 19:22:04 +0200

    first commit

---
+++ js/nav.js
@@ -0,0 +1,26 @@
+const createNav = () => {
+    let nav = document.querySelector('.navbar');
+
+    nav.innerHTML = `
+        <div class="nav">
+            <img src="image/logo_oryxs03.png" class="brand-logo" alt="">
+            <div class="nav-items">
+                <div class="search">
+                    <input type="text" class="search-box" placeholder="search brand, product">
+                    <button class="search-btn">search</button>
+                </div>
+                <a href="#"><i class="fa-light fa-basket-shopping"></i></a>
+                <a href="#"><i class="fa-light fa-user"></i></a>
+            </div>
+        </div>
+        <ul class="links-container">
+            <li class="link-item"><a href="#" class="link">home</a></li>
+            <li class="link-item"><a href="#" class="link">women</a></li>
+            <li class="link-item"><a href="#" class="link">men</a></li>
+            <li class="link-item"><a href="#" class="link">kids</a></li>
+            <li class="link-item"><a href="#" class="link">accessories</a></li>
+        </ul>
+    `;
+}
+
+createNav();
